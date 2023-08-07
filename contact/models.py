ef07fb46770ba3ec9e99c4a43f678e1c1bf4ba0c
Alina Boichenko <ilub1108@gmail.com>
2023-08-07 10:10:08 +0300

    Created models and migrations

--- contact/models.py
+++ contact/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+class User(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    email = models.EmailField(max_length=254, null=True, blank=True)
+
+    def __str__(self) -> str:
+        return self.email
+
+    class Meta:
+        ordering = ["first_name"]
